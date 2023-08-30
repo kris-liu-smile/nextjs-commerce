50d53bdb2c1fac96e8f1709a0caaaf72447609ce
Brian.Jiang2021 <brian.jiang@bundleb2b.net>
2023-08-30 16:21:08 +0800

    feat: headless init test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from 'components/layout/navbar';
 import { Inter } from 'next/font/google';
+import { Script } from 'next/script';
 import { ReactNode, Suspense } from 'react';
 import './globals.css';
 
@@ -37,6 +38,7 @@ const inter = Inter({
 export default async function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.variable}>
+      <Script data-storehash="imk8zz7jip" data-channelid="1222858" src="https://cdn.bundleb2b.net/b2b/staging/storefront/buyerPortal.js"></Script>
       <body className="bg-neutral-50 text-black selection:bg-teal-300 dark:bg-neutral-900 dark:text-white dark:selection:bg-pink-500 dark:selection:text-white">
         <Navbar />
         <Suspense>
